806e15db6f48a85960f0750efad92b8af2862961
Patricia Tirolla <patricia.tirolla@hyperisland.se>
2024-11-18 18:04:07 +0100

    Fix projects objects and project projectsUI
    The projects have now fixed properties and values.
    The Project constructor was deleted.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
+
 import "./styles/contact-section.css";
 import "./styles/form.css";
 import "./styles/projects-section.css";
 
+import { displayProjects } from "./projectsUI";
+
 console.log("It works!");
+
+displayProjects();
--- src/projectsUI.js
+++ src/projectsUI.js
@@ -1,17 +1,26 @@
-import { Project, projectList, addNewProjectToProjectList } from "./projects";
+import {projectList,  } from "./projects";
 
-export function createNewProjectCard() {
-    let projectsContainer = document.getElementById("projects-container");
-    projectsContainer.innerHTML = "";
-
-
+export function displayProjects() {
+    let projectsContainer = document.getElementById("projects-section");
+    
     projectList.forEach((project) => {
 
     
-    const projectTemplate = document.querySelector("project.template");
-    let clone = projectTemplate.conten.cloneNode(true);
+    const projectTemplate = document.querySelector(".project-template");
+    let clone = projectTemplate.content.cloneNode(true);
+    
+    clone.querySelector("h1").textContent = project.title;
+    clone.querySelector(".author").textContent = project.author;
+    clone.querySelector(".preview-link").setAttribute('href', project.previewLink); 
+    clone.querySelector(".code-link").setAttribute('href', project.codeLink);
 
-    // Unfinished - interact with the template
+    for (let skill of project.skills) {
+        let spanCreation = document.createElement("span");
+        spanCreation.textContent = skill;
+        clone.querySelector(".project-skills-container").appendChild(spanCreation);
+    }
 
+    projectsContainer.appendChild(clone);
     })
+
 }
